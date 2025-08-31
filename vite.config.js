4e0e2569077c87f0e972e492c7b28d5f470c61ab
Mehedi Hasan <mehedi.hasan.07@icloud.com>
2025-08-31 18:58:19 +0600

    Final Upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// Get the base URL from an environment variable, with a fallback to '/'
+const base = process.env.VITE_APP_BASE_URL || '/';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/nike-clone/',  // Update this to match your new repo name
-})
+  base: base,
+});
